9a10a5d3cb18801a26de2109cb89a7acca99cd3a
Stefan Johnson <sj@scrimpycat.io>
2018-09-13 23:04:43 +1000

    Add description

--- lib/resx/resource/content.ex
+++ lib/resx/resource/content.ex
@@ -1,4 +1,13 @@
 defmodule Resx.Resource.Content do
+    @moduledoc """
+      The content of a resource.
+
+        %Resx.Resource.Content{
+            type: :html,
+            data: "<p>Hello</p>"
+        }
+    """
+
     defstruct [:type, :data]
 
     alias Resx.Resource.Content
