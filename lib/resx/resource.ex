953ec1563af1ea682f110009516044f72343531b
Stefan Johnson <sj@scrimpycat.io>
2018-09-14 02:43:40 +1000

    Add meta field to resource

--- lib/resx/resource.ex
+++ lib/resx/resource.ex
@@ -1,11 +1,13 @@
 defmodule Resx.Resource do
-    defstruct [:content]
-
     alias Resx.Resource
+    alias Resx.Resource.Content
     alias Resx.Resource.Reference.Integrity
 
+    defstruct [content: %Content{}, meta: []]
+
     @type t :: %Resource{
-        content: Resource.Content.t
+        content: Content.t,
+        meta: keyword
     }
 
     @doc """
@@ -13,7 +15,7 @@ defmodule Resx.Resource do
 
       See `hash/2` for more information.
     """
-    @spec hash(t | Resource.Content.t) :: Integrity.checksum
+    @spec hash(t | Content.t) :: Integrity.checksum
     def hash(resource) do
         hash(resource, Application.get_env(:resx, :hash, :sha))
     end
@@ -41,7 +43,7 @@ defmodule Resx.Resource do
         iex> Resx.Resource.hash(%Resx.Resource.Content{ type: :string, data: "Hello" }, { :base64, &Base.encode64/1 })
         { :base64, "g2gCZAAGc3RyaW5nbQAAAAVIZWxsbw==" }
     """
-    @spec hash(t | Resource.Content.t, Integrity.algo | { Integrity.algo, fun | mfa | { module, atom, list } | { module, atom, list, non_neg_integer } }) :: Integrity.checksum
+    @spec hash(t | Content.t, Integrity.algo | { Integrity.algo, fun | mfa | { module, atom, list } | { module, atom, list, non_neg_integer } }) :: Integrity.checksum
     def hash(resource, { algo, { module, fun, args, index } }) do
         { algo, apply(module, fun, List.insert_at(args, index, to_binary(resource))) }
     end
@@ -59,5 +61,5 @@ defmodule Resx.Resource do
     end
 
     defp to_binary(%Resource{ content: content }), do: to_binary(content)
-    defp to_binary(%Resource.Content{ type: type, data: data }), do: :erlang.term_to_binary({ type, data })
+    defp to_binary(%Content{ type: type, data: data }), do: :erlang.term_to_binary({ type, data })
 end
