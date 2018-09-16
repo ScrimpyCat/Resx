8d005bfbdbaf8662d69161c1ec76160b36e36c87
Stefan Johnson <sj@scrimpycat.io>
2018-09-16 22:47:02 +1000

    Add callback for obtaining a resources URI

--- lib/resx/producer.ex
+++ lib/resx/producer.ex
@@ -5,6 +5,8 @@ defmodule Resx.Producer do
     @type resource_error :: :unknown_resource | :malformed_resource
     @type resource_attribute_key :: atom
 
+    @callback resource_uri(resource :: Resource.t) :: { :ok, result :: any } | error(resource_error)
+
     @callback get_resource_attribute(resource :: Resource.t, field :: resource_attribute_key) :: { :ok, result :: any } | error(resource_error | :unknown_key)
 
     @callback get_resource_attributes(resource :: Resource.t) :: { :ok, result :: %{ optional(resource_attribute_key) => any } } | error(resource_error)
