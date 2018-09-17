2d68179e7bbf197601625c14a78e34698664bade
Stefan Johnson <sj@scrimpycat.io>
2018-09-18 02:57:23 +1000

    Refine producer contract

--- lib/resx/producer.ex
+++ lib/resx/producer.ex
@@ -3,17 +3,21 @@ defmodule Resx.Producer do
     alias Resx.Resource.Reference
 
     @type error(type) :: { :error, { :internal | type, reason :: term } }
-    @type resource_error :: :unknown_resource | :malformed_resource
+    @type reference_error :: :invalid_reference
+    @type resource_error :: :unknown_resource
     @type resource_attribute_key :: atom
     @type uri :: String.t
+    @type ref :: uri | Reference.t
 
-    @callback open(uri) :: { :ok, resource :: Resource.t } | error(resource_error)
+    @callback open(ref) :: { :ok, resource :: Resource.t } | error(resource_error | reference_error)
 
-    @callback resource_uri(reference :: Reference.t) :: { :ok, result :: uri } | error(resource_error)
+    @callback exists?(ref) :: { :ok, exists :: boolean } | error(reference_error)
 
-    @callback get_resource_attribute(reference :: Reference.t, field :: resource_attribute_key) :: { :ok, result :: any } | error(resource_error | :unknown_key)
+    @callback resource_uri(Reference.t) :: { :ok, uri } | error(resource_error | reference_error)
 
-    @callback get_resource_attributes(reference :: Reference.t) :: { :ok, result :: %{ optional(resource_attribute_key) => any } } | error(resource_error)
+    @callback get_resource_attribute(ref, field :: resource_attribute_key) :: { :ok, attribute_value :: any } | error(resource_error | reference_error | :unknown_key)
 
-    @callback resource_attributes(reference :: Reference.t) :: { :ok, [field :: resource_attribute_key] } | error(resource_error)
+    @callback get_resource_attributes(ref) :: { :ok, attribute_values :: %{ optional(resource_attribute_key) => any } } | error(resource_error | reference_error)
+
+    @callback resource_attributes(ref) :: { :ok, [field :: resource_attribute_key] } | error(resource_error | reference_error)
 end
