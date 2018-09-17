3fe99bf3d160d20b0f3d57a3e82bacb90b1afd49
Stefan Johnson <sj@scrimpycat.io>
2018-09-17 13:35:22 +1000

    Add callback to open URI

--- lib/resx/producer.ex
+++ lib/resx/producer.ex
@@ -4,8 +4,11 @@ defmodule Resx.Producer do
     @type error(type) :: { :error, { :internal | type, reason :: term } }
     @type resource_error :: :unknown_resource | :malformed_resource
     @type resource_attribute_key :: atom
+    @type uri :: String.t
 
-    @callback resource_uri(resource :: Resource.t) :: { :ok, result :: any } | error(resource_error)
+    @callback open(uri) :: { :ok, resource :: Resource.t } | error(resource_error)
+
+    @callback resource_uri(resource :: Resource.t) :: { :ok, result :: uri } | error(resource_error)
 
     @callback get_resource_attribute(resource :: Resource.t, field :: resource_attribute_key) :: { :ok, result :: any } | error(resource_error | :unknown_key)
 
