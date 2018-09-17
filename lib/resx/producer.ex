69effb65237c5abcc95da7decc602454137d15e9
Stefan Johnson <sj@scrimpycat.io>
2018-09-18 02:33:31 +1000

    Change input to reference

--- lib/resx/producer.ex
+++ lib/resx/producer.ex
@@ -1,5 +1,6 @@
 defmodule Resx.Producer do
     alias Resx.Resource
+    alias Resx.Resource.Reference
 
     @type error(type) :: { :error, { :internal | type, reason :: term } }
     @type resource_error :: :unknown_resource | :malformed_resource
@@ -8,11 +9,11 @@ defmodule Resx.Producer do
 
     @callback open(uri) :: { :ok, resource :: Resource.t } | error(resource_error)
 
-    @callback resource_uri(resource :: Resource.t) :: { :ok, result :: uri } | error(resource_error)
+    @callback resource_uri(reference :: Reference.t) :: { :ok, result :: uri } | error(resource_error)
 
-    @callback get_resource_attribute(resource :: Resource.t, field :: resource_attribute_key) :: { :ok, result :: any } | error(resource_error | :unknown_key)
+    @callback get_resource_attribute(reference :: Reference.t, field :: resource_attribute_key) :: { :ok, result :: any } | error(resource_error | :unknown_key)
 
-    @callback get_resource_attributes(resource :: Resource.t) :: { :ok, result :: %{ optional(resource_attribute_key) => any } } | error(resource_error)
+    @callback get_resource_attributes(reference :: Reference.t) :: { :ok, result :: %{ optional(resource_attribute_key) => any } } | error(resource_error)
 
-    @callback resource_attributes(resource :: Resource.t) :: { :ok, [field :: resource_attribute_key] } | error(resource_error)
+    @callback resource_attributes(reference :: Reference.t) :: { :ok, [field :: resource_attribute_key] } | error(resource_error)
 end
