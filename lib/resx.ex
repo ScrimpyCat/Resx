0bd6389b8ccb5533b4ef4980993f373d5c6b9de6
Stefan Johnson <sj@scrimpycat.io>
2018-09-22 23:11:19 +1000

    Get producer for URI

--- lib/resx.ex
+++ lib/resx.ex
@@ -1,2 +1,15 @@
 defmodule Resx do
+    @default_producers %{
+        "file" => Resx.Producers.File
+    }
+
+    def producer_for_uri(uri) do
+        %{ scheme: scheme } = URI.parse(uri)
+
+        Map.merge(@default_producers, Application.get_env(:resx, :producers, %{}))
+        |> case do
+            %{ ^scheme => adapter } -> adapter
+            _ -> nil
+        end
+    end
 end
