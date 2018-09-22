a759709630a110d88fa7c69a242f65b4e1c2e673
Stefan Johnson <sj@scrimpycat.io>
2018-09-23 00:22:40 +1000

    Get producer for resource or reference

--- lib/resx.ex
+++ lib/resx.ex
@@ -1,9 +1,14 @@
 defmodule Resx do
+    alias Resx.Resource
+    alias Resx.Resource.Reference
+
     @default_producers %{
         "file" => Resx.Producers.File
     }
 
-    def producer_for_uri(uri) do
+    def producer(%Resource{ reference: reference }), do: producer(reference)
+    def producer(%Reference{ adapter: adapter }), do: adapter
+    def producer(uri) do
         %{ scheme: scheme } = URI.parse(uri)
 
         Map.merge(@default_producers, Application.get_env(:resx, :producers, %{}))
