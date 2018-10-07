be44e502e0a9f71b653123b3f38dbe7b7e069fcb
Stefan Johnson <sj@scrimpycat.io>
2018-10-07 19:51:07 +1100

    Move transformation apply

--- lib/resx/transformer.ex
+++ lib/resx/transformer.ex
@@ -1,5 +1,15 @@
 defmodule Resx.Transformer do
     alias Resx.Resource
+    alias Resx.Resource.Reference
+    alias Resx.Resource.Reference.Integrity
 
     @callback transform(Resource.t) :: { :ok, resource :: Resource.t } | Resx.error
+
+    def apply(resource, transformer) do
+        case transformer.transform(resource) do
+            { :ok, resource = %{ reference: reference } } ->
+                { :ok, %{ resource | reference: %Reference{ adapter: Resx.Producers.Transform, repository: { transformer, reference }, integrity: %Integrity{ checksum: Resource.hash(resource), timestamp: DateTime.to_unix(DateTime.utc_now) } } } }
+            { :error, error } -> { :error, error }
+        end
+    end
 end
