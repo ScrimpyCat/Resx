39c137675eb61d7d3f17ba593c7ed8651e73e91f
Stefan Johnson <sj@scrimpycat.io>
2018-10-25 22:53:19 +1100

    Add exception variation of apply!/2

--- lib/resx/transformer.ex
+++ lib/resx/transformer.ex
@@ -1,9 +1,20 @@
 defmodule Resx.Transformer do
+    import Kernel, except: [apply: 2]
+
     alias Resx.Resource
     alias Resx.Resource.Reference
     alias Resx.Resource.Reference.Integrity
 
     @callback transform(Resource.t) :: { :ok, resource :: Resource.t } | Resx.error
+
+    defmodule TransformError do
+        defexception [:message, :type, :reason]
+
+        @impl Exception
+        def exception({ type, reason }) do
+            %TransformError{ message: "failed to transform resource due to #{type} error: #{inspect reason}", type: type, reason: reason }
+        end
+    end
 
     @doc """
       Apply a transformation to a resource.
@@ -17,6 +28,13 @@ defmodule Resx.Transformer do
             { :ok, resource = %{ reference: reference } } ->
                 { :ok, %{ resource | reference: %Reference{ adapter: Resx.Producers.Transform, repository: { transformer, reference }, integrity: %Integrity{ checksum: Resource.hash(resource), timestamp: DateTime.to_unix(DateTime.utc_now) } } } }
             { :error, error } -> { :error, error }
+        end
+    end
+
+    def apply!(resource, transformer) do
+        case apply(resource, transformer) do
+            { :error, error } -> raise TransformError, error
+            result -> result
         end
     end
 end
