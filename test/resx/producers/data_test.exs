d3ea038396311320ee6a9f0c83ce438d4543ea9d
Stefan Johnson <sj@scrimpycat.io>
2018-11-02 16:47:55 +1100

    Test data producer's exists?

--- test/resx/producers/data_test.exs
+++ test/resx/producers/data_test.exs
@@ -1,4 +1,11 @@
 defmodule Resx.Producers.DataTest do
     use ExUnit.Case
     doctest Resx.Producers.Data
+
+    alias Resx.Resource
+
+    test "exists?/1" do
+        assert { :ok, true } == Resource.exists?("data:,test")
+        assert { :ok, true } == Resource.exists?("data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D")
+    end
 end
