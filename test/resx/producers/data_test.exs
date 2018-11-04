ebd31f758342bb8d5289847c99a36ce4a8d4ee98
Stefan Johnson <sj@scrimpycat.io>
2018-11-04 20:37:57 +1100

    Test data producer's alike?

--- test/resx/producers/data_test.exs
+++ test/resx/producers/data_test.exs
@@ -8,4 +8,13 @@ defmodule Resx.Producers.DataTest do
         assert { :ok, true } == Resource.exists?("data:,test")
         assert { :ok, true } == Resource.exists?("data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D")
     end
+
+    test "alike?/2" do
+        assert true == Resource.alike?("data:,test", "data:,test")
+        assert true == Resource.alike?("data:,test", "data:text/plain;charset=US-ASCII,test")
+        assert true == Resource.alike?("data:,test", "data:text/plain;charset=US-ASCII;base64,dGVzdA==")
+        assert false == Resource.alike?("data:,test", "data:text/plain;charset=US-ASCII;base64,SGVsbG8sIFdvcmxkIQ%3D%3D")
+        assert false == Resource.alike?("data:,test", "data:text/plain;base64,dGVzdA==")
+        assert false == Resource.alike?("data:,test", "data:base64,dGVzdA==")
+    end
 end
