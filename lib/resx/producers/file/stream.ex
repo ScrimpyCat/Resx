6a945deff5f1838501668b264f8bc06a8b57d6d8
Stefan Johnson <sj@scrimpycat.io>
2018-11-28 13:10:19 +1100

    Set call options

--- lib/resx/producers/file/stream.ex
+++ lib/resx/producers/file/stream.ex
@@ -11,16 +11,16 @@ defmodule Resx.Producers.File.Stream do
     }
 
     defimpl Collectable do
-        def into(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Collectable, :into, [stream])
+        def into(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Collectable, :into, [stream], path: stream.path, exception: true)
     end
 
     defimpl Enumerable do
-        def count(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Enumerable, :count, [stream])
+        def count(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Enumerable, :count, [stream], path: stream.path, exception: true)
 
-        def member?(%{ stream: stream, node: node }, element), do: Resx.Producers.File.call(node, Enumerable, :member?, [stream, element])
+        def member?(%{ stream: stream, node: node }, element), do: Resx.Producers.File.call(node, Enumerable, :member?, [stream, element], path: stream.path, exception: true)
 
-        def reduce(%{ stream: stream, node: node }, acc, reducer), do: Resx.Producers.File.call(node, Enumerable, :reduce, [stream, acc, reducer])
+        def reduce(%{ stream: stream, node: node }, acc, reducer), do: Resx.Producers.File.call(node, Enumerable, :reduce, [stream, acc, reducer], path: stream.path, exception: true)
 
-        def slice(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Enumerable, :slice, [stream])
+        def slice(%{ stream: stream, node: node }), do: Resx.Producers.File.call(node, Enumerable, :slice, [stream], path: stream.path, exception: true)
     end
 end
