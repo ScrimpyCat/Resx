e181ccd567a558310ee1ce4695441248efa50d8e
Stefan Johnson <sj@scrimpycat.io>
2018-12-05 03:00:22 +1100

    Fix up file uri errors

--- lib/resx/producers/file.ex
+++ lib/resx/producers/file.ex
@@ -193,9 +193,10 @@ defmodule Resx.Producers.File do
     end
 
     defp to_path(%Reference{ repository: { node, path, source } }), do: check_access(node, path, source)
+    defp to_path(%URI{ scheme: "file", path: nil }), do: { :error, { :invalid_reference, "no file path was specified" } }
     defp to_path(%URI{ scheme: "file", host: host, path: path, userinfo: nil, query: query }) when host in [nil, "localhost"], do: check_access(node(), path, source(query))
-    defp to_path(%URI{ scheme: "file", host: host, path: path, userinfo: user, query: query }), do: check_access(String.to_atom(user <> "@" <> host), path, source(query))
-    defp to_path(%URI{ scheme: "file" }), do: { :error, "only supports local or remote node file references" }
+    defp to_path(%URI{ scheme: "file", host: host, path: path, userinfo: user, query: query }) when not is_nil(user), do: check_access(String.to_atom(user <> "@" <> host), path, source(query))
+    defp to_path(%URI{ scheme: "file" }), do: { :error, { :invalid_reference, "only supports local or remote node file references" } }
     defp to_path(uri) when is_binary(uri), do: URI.decode(uri) |> URI.parse |> to_path
     defp to_path(_), do: { :error, { :invalid_reference, "not a file reference" } }
 
